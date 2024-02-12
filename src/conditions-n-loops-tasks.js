7980191ac95cad294edb855c445a37364c2b37e9
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2024-02-12 14:09:52 +0300

    feat: solve shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -404,8 +404,24 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let resultString = str;
+
+  for (let iter = 0; iter < iterations; iter += 1) {
+    let left = '';
+    let right = '';
+
+    for (let i = 0; i < resultString.length; i += 1) {
+      if (i % 2 === 0) {
+        left += resultString[i];
+      } else {
+        right += resultString[i];
+      }
+    }
+    resultString = left + right;
+  }
+
+  return resultString;
 }
 
 /**
